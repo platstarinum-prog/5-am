75129193cf801e9edf921c481da8ceb50ebd3c8d
platstarinum-prog <platstarinum@gmail.com>
2026-05-19 21:45:20 +0300

    Update App.tsx

--- project/src/App.tsx
+++ project/src/App.tsx
@@ -1,4 +1,7 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+// Добавь этот импорт:
+import { LanguageProvider } from './context/LanguageContext'; 
+
 import Navbar from './components/Navbar';
 import LanguageSwitcher from './components/LanguageSwitcher';
 import HomePage from './pages/HomePage';
@@ -8,17 +11,19 @@ import FaqPage from './pages/FaqPage';
 
 export default function App() {
   return (
-    <BrowserRouter>
-      <div className="min-h-screen bg-black text-white font-sans antialiased">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/catalog" element={<CatalogPage />} />
-          <Route path="/reviews" element={<ReviewsPage />} />
-          <Route path="/faq" element={<FaqPage />} />
-        </Routes>
-        <LanguageSwitcher />
-      </div>
-    </BrowserRouter>
+    <LanguageProvider>
+      <BrowserRouter>
+        <div className="min-h-screen bg-black text-white font-sans antialiased">
+          <Navbar />
+          <LanguageSwitcher />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/catalog" element={<CatalogPage />} />
+            <Route path="/reviews" element={<ReviewsPage />} />
+            <Route path="/faq" element={<FaqPage />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </LanguageProvider>
   );
 }
